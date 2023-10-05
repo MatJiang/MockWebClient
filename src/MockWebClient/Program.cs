2808a9a8c0b811e42afa470aa3de72f2a397c72b
江馬特 <40130808+MatJiang@users.noreply.github.com>
2023-10-05 09:51:42 -0500

    add:加入自動更新chrome driver的參考套件 (#2)
    Co-authored-by: Mat Jiang <mat.jiang@esi-tech.net>

--- src/MockWebClient/Program.cs
+++ src/MockWebClient/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Security.Cryptography;
 using MockWebClient;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
 var users = RandomNumberGenerator.GetInt32(1, 100);
-
+new DriverManager().SetUpDriver(new ChromeConfig());
 BrowseRepository.Default.DoBrowse(users);
